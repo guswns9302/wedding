4e51c18caea91ad3da585547d9035a3627aa6e00
YHJ <guswns930213@gmail.com>
2022-11-09 17:48:00 +0900

    스와이퍼 추가

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,9 @@
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
-
+    function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+    function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
+    function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
@@ -51,4 +53,63 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    var camelize = function camelize(str) {
+        var text = str.replace(/[-_\s.]+(.)?/g, function (_, c) {
+            return c ? c.toUpperCase() : '';
+        });
+        return "".concat(text.substr(0, 1).toLowerCase()).concat(text.substr(1));
+    };
+
+    var getData = function getData(el, data) {
+        try {
+            return JSON.parse(el.dataset[camelize(data)]);
+        } catch (e) {
+            return el.dataset[camelize(data)];
+        }
+    };
+
+    var swipers = document.querySelectorAll('[data-swiper]');
+    var navbarVerticalToggle = document.querySelector('.navbar-vertical-toggle');
+    swipers.forEach(function (swiper) {
+        var options = getData(swiper, 'swiper');
+        var thumbsOptions = options.thumb;
+        var thumbsInit;
+
+        if (thumbsOptions) {
+            var thumbImages = swiper.querySelectorAll('img');
+            var slides = '';
+            thumbImages.forEach(function (img) {
+                slides += "\n          <div class='swiper-slide '>\n            <img class='img-fluid rounded mt-1' src=".concat(img.src, " alt=''/>\n          </div>\n        ");
+            });
+            var thumbs = document.createElement('div');
+            thumbs.setAttribute('class', 'swiper-container thumb');
+            thumbs.innerHTML = "<div class='swiper-wrapper'>".concat(slides, "</div>");
+
+            if (thumbsOptions.parent) {
+                var parent = document.querySelector(thumbsOptions.parent);
+                parent.parentNode.appendChild(thumbs);
+            } else {
+                swiper.parentNode.appendChild(thumbs);
+            }
+
+            thumbsInit = new window.Swiper(thumbs, thumbsOptions);
+        }
+
+        var swiperNav = swiper.querySelector('.swiper-nav');
+        var newSwiper = new window.Swiper(swiper, _objectSpread(_objectSpread({}, options), {}, {
+            navigation: {
+                nextEl: swiperNav === null || swiperNav === void 0 ? void 0 : swiperNav.querySelector('.swiper-button-next'),
+                prevEl: swiperNav === null || swiperNav === void 0 ? void 0 : swiperNav.querySelector('.swiper-button-prev')
+            },
+            thumbs: {
+                swiper: thumbsInit
+            }
+        }));
+
+        if (navbarVerticalToggle) {
+            navbarVerticalToggle.addEventListener('navbar.vertical.toggle', function () {
+                newSwiper.update();
+            });
+        }
+    });
 });
